720895a54c120fd0085b4f0030b3ea4410ed0c23
markc <markc@ACSAKVARIZEN>
2022-11-23 12:26:44 +0100

    Add project files.

---
+++ PascalWeb/wwwroot/geek.js
@@ -0,0 +1,12 @@
+﻿window.onload =
+    fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letoltesBefe(data)
+    );
+
+var viccek;
+
+function letoltesBefe(d) {
+    console.log("Sikeres letöltés");
+    console.log(d);
+}
